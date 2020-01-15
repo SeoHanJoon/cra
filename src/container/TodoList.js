11572d665fa0628560ca6f9973119f705423b467
SeoHanJoon <joon9817@naver.com>
2020-01-15 15:35:35 +0900

    cra study

---
+++ src/container/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoItem from '../components/TodoItem';
+
+function TodoList() {
+  return (
+    <>
+      <div>TodoList</div>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </>
+  );
+}
+
+export default TodoList;

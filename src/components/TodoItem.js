095a5623e25c20826d2aeb13fd56f68adcc0e841
SeoHanJoon <joon9817@naver.com>
2020-01-15 16:07:52 +0900

    life cycle

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
 class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    let a;
+  }
+
+  componentDidMount() {
+    this.a = setTimeout(() => {
+      console.log('setTime');
+    }, 3000);
+  }
+
+  componentWillUnmount() {
+    const { aaa } = this.props;
+    console.log(`${aaa}컴포넌트 지워짐`);
+    clearTimeout(this.a);
+  }
+
   render() {
+    const { aaa, content } = this.props;
+
     return (
       <div>
         <p>TodoItem</p>
+        <p>제목 : {aaa}</p>
+        <p>내용 : {content}</p>
       </div>
     );
   }
--- src/container/TodoList.js
+++ src/container/TodoList.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import TodoItem from '../components/TodoItem';
 
-function TodoList() {
-  return (
-    <>
-      <div>TodoList</div>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </>
-  );
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataList: [],
+    };
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    const data = [
+      { title: 'todo 1번', content: '내용1' },
+      { title: 'todo 2번', content: '내용2' },
+      { title: 'todo 3번', content: '내용3' },
+    ];
+    // const data = axios.get('https://api.blah.com/getData');
+    this.setState({
+      dataList: data,
+    });
+  }
+
+  func = () => {
+    this.setState({
+      dataList: [],
+    });
+  };
+
+  render() {
+    console.log('todoList render');
+    const { dataList } = this.state;
+
+    return (
+      <>
+        <button type="button" onClick={this.func}>
+          BUTTON
+        </button>
+        <div>TodoList</div>
+        {dataList.map((data) => (
+          <TodoItem aaa={data.title} content={data.content} />
+        ))}
+      </>
+    );
+  }
 }
 
 export default TodoList;
